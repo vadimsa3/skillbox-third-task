b4a6cfe5cdd7e36fa378b5de479a09130c7f6a34
vadimsa3 <vadimsa3@gmail.com>
2024-05-26 15:23:19 +0300

    database correct

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM openjdk:18-oracle
 WORKDIR /app
 
 # копирование JAR-файла (созданого gradle/build/bootJar) приложения в контейнер и переименование его
-COPY build/libs/skillbox-third-task-0.0.1-SNAPSHOT.jar contacts-app.jar
+COPY build/libs/skillbox-third-task-0.0.1-SNAPSHOT-plain.jar contacts-app.jar
 
 ## определение переменной среды, по умолчанию будет запуск со свойством из properties - true
 #ENV CREATE_ON_STARTUP=true
--- docker/init.sql
+++ docker/init.sql
@@ -6,16 +6,18 @@ CREATE SCHEMA IF NOT EXISTS contacts_schema;
 -- Создание таблицы contact в схеме contacts_schema
 CREATE TABLE IF NOT EXISTS contacts_schema.contact
 (
-    id BIGINT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL
+--    id BIGINT PRIMARY KEY,
+--    first_name VARCHAR(255) NOT NULL,
+--    last_name VARCHAR(255) NOT NULL,
+--    email VARCHAR(255) NOT NULL,
+--    phone VARCHAR(255) NOT NULL
 
---      id BIGINT PRIMARY KEY,
---      firstName VARCHAR(255) NOT NULL,
---      lastName VARCHAR(255) NOT NULL,
---      email VARCHAR(255) NOT NULL,
---      phone VARCHAR(255) NOT NULL
+      id BIGINT PRIMARY KEY,
+      "firstName" VARCHAR(255) NOT NULL,
+      "lastName" VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      phone VARCHAR(255) NOT NULL
 );
+
+
 
